81261f7b736a3894a2955ea5949b8a4f52880098
Jayaku Briliantio <51906567+ukayaj620@users.noreply.github.com>
2021-12-20 00:09:19 +0700

    Feat/real time mask detector (#1)
    * chore: ignore .vscode
    
    * feat: training mask detector model script using tensorflow
    
    * feat: trained mask detector model weight
    
    * feat: realtime mask detector

--- main.py
+++ main.py
@@ -1,28 +1,55 @@
-from mtcnn import MTCNN
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 import cv2
+import numpy as np
+from mtcnn import MTCNN
 
-face_detector = MTCNN()
+
 camera = cv2.VideoCapture(0)
+mtcnn_face_detector = MTCNN()
+mask_detector = load_model("./models/training_01/classifier_mobile_net_v2.h5")
+
+
+def preprocess_face(frame, face):
+    (x, y, w, h) = face
+    extracted_face = frame[y:(y + h), x:(x + w)]
+    extracted_face = cv2.resize(extracted_face, (224, 224))
+    expanded_face = np.expand_dims(extracted_face, axis=0)
+    return preprocess_input(expanded_face)
+
 
 def capture_face():
-  while True:
-    ret, frame = camera.read()
+    while True:
+        ret, frame = camera.read()
+
+        faces = mtcnn_face_detector.detect_faces(frame)
+
+        for face in faces:
+            (x, y, w, h) = face['box']
+            (mask_confidence, non_mask_confidence) = mask_detector.predict(
+                preprocess_face(frame, face['box']))[0]
+
+            if mask_confidence > non_mask_confidence:
+                confidence = round(mask_confidence * 100, 2)
+                label = "Mask, Confidence: {}%".format(confidence)
+                color = (0, 255, 0)
+            else:
+                confidence = round(non_mask_confidence * 100, 2)
+                label = "No Mask, Confidence: {}%".format(confidence)
+                color = (0, 0, 255)
+            
+            cv2.putText(frame, label, (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+
+        cv2.imshow("Maskya", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
 
 
-    faces = face_detector.detect_faces(frame)
-
-    for face in faces:
-      (x, y, w, h) = face['box']
-      cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    cv2.imshow("Maskya", frame)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
-  
-  camera.release()
-  cv2.destroyAllWindows()
-
-
-if __name__=="__main__":
-  capture_face()
+if __name__ == "__main__":
+    capture_face()
